70c0c10894389350c6a3f8afac7bfede0e0f2666
JIHOJU <jihoju96@gmail.com>
2022-10-17 23:03:16 +0900

    feat: add logic to save Github repo's commit to csv
    사실 다른 수정이 필요한 코드도 추가,,,담부턴 다 나누자ㅠ

--- shell.py
+++ shell.py
@@ -1,19 +1,12 @@
+import time
+import requests
 import click
 from collections import OrderedDict
 from github import Github
 import json
-from urllib.request import Request, urlopen
 import sys
 import csv
 import random
-
-URL = "https://api.github.com/users/"
-
-""" url 로 github api 호출
-    def get_user_info(self):
-        headers = {'Authorization': "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"}
-        user_data = requests.get(URL + self.github_id, headers=headers).json()
-        pprint(user_data)"""
 
 
 def write_commit_info_in_json(data, path):
@@ -47,17 +40,23 @@ class GitHubAPIShell:
     """
 
     def __init__(self, argv):
-        self.g = Github(login_or_token="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")  # github api object
+        self.g = Github(login_or_token="<token>")  # github api object
+        self.header = {'Accept': 'application/vnd.github.v3+json',
+                       'Authorization': 'Bearer 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'}
         self.filepath = argv  # argv: Github ID를 담은 파일 경로
         self.user = None  # GitHub user 객체 담기 위함
         self.languages_url = list()
         self.repo_objs = list()  # repo 객체 담기 위함 -> Commit data 를 가져오기 위함
 
     def run(self):
+        start_time = time.time()
         gen = read_file(self.filepath)  # 파일 데이터 generator
-        # write_commit_info_in_json(gen)  # 유저 정보 csv 파일에 저장
+        # self.write_user_info_in_csv(gen)  # 유저 정보 csv 파일에 저장
         self.write_repository_info_in_csv(gen)
-        self.write_language_info_in_csv()
+        # self.write_language_info_in_csv()
+        self.write_commit_info_in_csv()
+        end_time = time.time()
+        print(round(end_time - start_time, 3))
 
         return 0
 
@@ -65,17 +64,17 @@ class GitHubAPIShell:
         with open('./member.csv', 'w') as f:
             wr = csv.writer(f)
             wr.writerow(["Github_id", "Avatar_url", "User_name", "Company", "Bio", "Location", "User_github_url", \
-                         "Followers", "Level", "Group_cnt", "User_rank_id", "Super_github_id"])
+                         "Ghchart_url", "Followers", "Level", "Group_cnt", "User_rank_id", "Super_github_id"])
             for github_id in gen:
                 self.user = self.g.get_user(github_id)
                 wr.writerow([value for _, value in self.get_user_info().items()])
 
     def write_repository_info_in_csv(self, gen):
-        with open('./repository.csv', 'w') as f:
+        with open('./repository2.csv', 'w') as f:
             wr = csv.writer(f)
             wr.writerow(
                 ["Repository_id", "Repo_name", "Repo_url", "Fork_count", "Stargazers_count", "Created_at", "Updated_at",
-                 "Github_id", "Repo_rank_id"])
+                 "Github_id", "Repo_rank_id", "Issue_count", "Pr_count", "Commit_count"])
 
             repo_id = 1  # 순차적으로 repo id를 주기 위함
             for github_id in gen:
@@ -83,15 +82,28 @@ class GitHubAPIShell:
                 for _, values in self.get_repo_info().items():
                     wr.writerow([repo_id] + [value for _, value in values.items()])
                     repo_id += 1
+                print(github_id)
+            print("-----")
 
     def write_language_info_in_csv(self):
-        with open('./language.csv', 'w') as f:
+        with open('./language2.csv', 'w') as f:
             wr = csv.writer(f)
             wr.writerow(["Language", "Repo_id", "Language_byte"])
 
             for repo_name, lang_url in self.languages_url:
                 for lang, byte in self.get_language_info(lang_url).items():
                     wr.writerow([lang, repo_name, byte])
+
+    def write_commit_info_in_csv(self):
+        with open('./commit.csv', 'w') as f:
+            wr = csv.writer(f)
+            wr.writerow(["Commit_id", "Commit_msg", "Author", "Date", "Commit_url", "Repository_id"])
+
+            commit_id = 1  # 순차적으로 commit id를 주기 위함
+            for github_id, username, repo in self.repo_objs:
+                for commit_info in self.get_commits(github_id, username, repo):
+                    wr.writerow([commit_id] + [val for _, val in commit_info.items()])
+                    commit_id += 1
 
     def get_user_info(self):
 
@@ -105,7 +117,7 @@ class GitHubAPIShell:
         user_infos["bio"] = self.user.bio
         user_infos["location"] = self.user.location
         user_infos["html_url"] = self.user.html_url
-        # user_infos["ghchart_url"] = f"https://ghchart.rshah.org/{self.github_id}"
+        user_infos["ghchart_url"] = f"https://ghchart.rshah.org/{self.user.login}"
         user_infos["followers"] = self.user.followers
         user_infos["level"] = 1
         user_infos["group_cnt"] = 0
@@ -131,7 +143,8 @@ class GitHubAPIShell:
 
         for repo in repos:
             self.languages_url.append((repo.name, repo.languages_url))
-            self.repo_objs.append(repo)
+            self.repo_objs.append((self.user.login, self.user.name, repo))
+
             repo_infos[repo.full_name] = {
                 "Repo_name": repo.name,
                 "Repo_url": repo.svn_url,
@@ -141,6 +154,11 @@ class GitHubAPIShell:
                 "Updated_at": repo.updated_at,
                 "Github_id": self.user.login,
                 "Repo_rank_id": 1,
+                "Issue_count": len(requests.get(repo.issues_url.replace("{/number}", "") + "?state=all",
+                                                headers=self.header).json()),
+                "Pr_count": len(requests.get(repo.pulls_url.replace("{/number}", "") + "?state=all",
+                                             headers=self.header).json()),
+                "Commit_count": 1
                 # "updatedAt": "{0}-{1}-{2} {3}".format(updated.year, updated.month, updated.day,
                 #                                       updated.time()),
                 # "language_url": repo.languages_url,
@@ -150,7 +168,7 @@ class GitHubAPIShell:
         return repo_infos
 
     @staticmethod
-    def get_commits(repo):
+    def get_commits(githubid, username, repo):
         """
         커밋 메시지
         커밋터
@@ -164,55 +182,62 @@ class GitHubAPIShell:
 
         try:
             for commit in repo.get_commits():
-                commit_date = commit.commit.author.date
+                if commit.commit.committer.name == "GitHub" or \
+                        (commit.commit.author.name.lower() != githubid.lower()
+                         and commit.commit.author.name.lower() != username.lower()
+                         and commit.commit.committer.name.lower() != githubid.lower()
+                         and commit.commit.committer.name.lower() != username.lower()):
+                    continue
+
                 commit_message = " ".join(commit.commit.message.split())
-
                 data = {
-                    "message": commit_message,
-                    "committer": commit.commit.committer.name,
-                    "author": commit.commit.author.name,
-                    "date": "{0}-{1}-{2} {3}".format(commit_date.year, commit_date.month, commit_date.day,
-                                                     commit_date.time())
+                    "Commit_msg": commit_message,
+                    "Author": commit.commit.author.name,
+                    "Date": commit.commit.author.date,
+                    "Commit_url": commit.commit.html_url,
+                    "Repository_id": repo.full_name,
+                    # "date": "{0}-{1}-{2} {3}".format(commit_date.year, commit_date.month, commit_date.day,
+                    #                                  commit_date.time())
                 }
-
                 return_data.append(data)
         except github.GithubException as e:
             print(repo.full_name, e)
 
         return return_data
 
-    @staticmethod
-    def get_language_info(url):
+    def get_language_info(self, url):
         """ 각 레포 마다 사용된 언어 정보
 
         :return: 입력 깃헙 ID 의 public repo 에 사용된 언어 정보를 dict 자료 형태로 리턴
         """
 
-        urlTicker = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-        return json.loads(urlopen(urlTicker).read())
+        res = requests.get(url, headers=self.header)
 
-    def get_language_stat(self):
-        """ 사용자 언어 통계량 계산
+        return res.json()
 
-        :return: 깃헙 유저의 사용 언어별 퍼센트
-        """
-        langs = dict()
-        lang_sum = 0
-        for url in self.languages_url:
-            repo_langs = json.loads(urllib.request.urlopen(url).read())
-            for lang in repo_langs.keys():
-                if lang in langs:
-                    langs[lang] += int(repo_langs[lang])
-                else:
-                    langs[lang] = int(repo_langs[lang])
-                lang_sum += int(repo_langs[lang])
 
-        for lang in langs.keys():
-            langs[lang] = round((langs[lang] / lang_sum) * 100, 3)
+def get_language_stat(self):
+    """ 사용자 언어 통계량 계산
 
-        sorted_dict = sorted(langs.items(), key=lambda item: item[1], reverse=True)
+    :return: 깃헙 유저의 사용 언어별 퍼센트
+    """
+    langs = dict()
+    lang_sum = 0
+    for url in self.languages_url:
+        repo_langs = requests.get(url, headers=self.header).json()
+        for lang in repo_langs.keys():
+            if lang in langs:
+                langs[lang] += int(repo_langs[lang])
+            else:
+                langs[lang] = int(repo_langs[lang])
+            lang_sum += int(repo_langs[lang])
 
-        return sorted_dict
+    for lang in langs.keys():
+        langs[lang] = round((langs[lang] / lang_sum) * 100, 3)
+
+    sorted_dict = sorted(langs.items(), key=lambda item: item[1], reverse=True)
+
+    return sorted_dict
 
 
 @click.command()
